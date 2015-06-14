46d1d0b4f7317cf9f5fe8922c5a73f69e75506af
mehrdadrafiee <rafiee.mehrdad@gmail.com>
2015-06-13 23:55:03 -0500

    basic outline of the GameManager service added.

--- app/scripts/game/game.js
+++ app/scripts/game/game.js
@@ -1,1 +1,11 @@
-angular.module('Game', []);
+angular.module('Game', [])
+  .service('GameManager', function () {
+  	//Create aa new game
+  	this.newGame = function() {};
+  	//Handle the move action() {};
+  	this.move = function() {};
+  	//Update the score
+  	this.UpdateScore = function(newScore) {};
+  	//Are there moves left?
+  	this.movesAvailable = function() {};
+  });
